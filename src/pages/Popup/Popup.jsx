3cbdee9c8127cdec496a3c31dc367ee876c1488b
hunhunseunghun <hunhunseunghun@gmail.com>
2022-02-24 15:50:31 +0900

    doing :)

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import './Popup.css';
 
 const Popup = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      {' '}
+      <nav>코인시세보기프로그램</nav>
+      <main>Hello, world!</main>
+      <footer>peterkimzz.com</footer>{' '}
     </div>
   );
 };
