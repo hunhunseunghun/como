4c2ee2b9f356001c7447849198104541ed180315
hunhunseunghun <hunhunseunghun@gmail.com>
2022-02-28 17:42:52 +0900

    sperated krw market btc market doing :)

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -24,11 +24,14 @@ const Popup = () => {
     });
   };
 
+  const [initUpbitCryptos, setInitUpbitCryptos] = useState({});
+  const [websocketParams, setWebSocketParams] = useState([]);
+
   useEffect(async () => {
     try {
       const forUpdateCryptosObj = {}; // 업데이트되는 코인 정보, 탐색 성능 위해 객체 선택
       const { data: upbitSymbols } = await getUpbitSymbols();
-
+      console.log('forUpdateCryptosObj', forUpdateCryptosObj);
       //upbit 등록 종목명 krw,btc 필터
       upbitSymbols.forEach((ele) => {
         if (ele.market.indexOf(renderKRW) === -1) {
@@ -42,6 +45,7 @@ const Popup = () => {
       const { data: upbitTickers } = await getUpbitTickers(upbitMarkets); // 시세 정보
 
       // upbit ticker 객체에 업데이트
+
       upbitTickers.forEach((ele) => {
         if (ele.market.indexOf(renderKRW) === -1) {
           return;
@@ -50,52 +54,95 @@ const Popup = () => {
         }
       });
 
-      let initRenderCryptosArr = [];
-      const webSocketParam = [];
+      console.log('forUpdateCryptosObj', forUpdateCryptosObj);
 
-      for (let coin in forUpdateCryptosObj) {
-        initRenderCryptosArr.push(forUpdateCryptosObj[coin]);
-        webSocketParam.push(coin);
-      }
+      setWebSocketParams(upbitMarkets);
+      setInitUpbitCryptos(forUpdateCryptosObj);
+
+      // let initRenderCryptosArr = [];
+      // const webSocketParam = [];
+
+      // for (let coin in forUpdateCryptosObj) {
+      //   initRenderCryptosArr.push(forUpdateCryptosObj[coin]);
+      //   webSocketParam.push(coin);
+      // }
 
       //업비트 웹소켓 데이터 전송이 많은 경우에는 빠르고 비용이 적은 표준 WebSocket
       //연결된 소켓 세밀하게 관리 요할시 socket.io 업비트 사용 x
-      const socket = new WebSocket('wss://api.upbit.com/websocket/v1');
+      // const socket = new WebSocket('wss://api.upbit.com/websocket/v1');
 
-      socket.onopen = () => {
-        socket.send(
-          `${JSON.stringify([
-            { ticket: uuidv4() }, //for websocket handshaking , uuid4 library 랜덤키 생성
-            { type: 'ticker', codes: webSocketParam }, // codes 요청 코인 문자열  (ex. KRW-BTC, BTC-ETH)
-          ])}`
-        );
-      };
+      // socket.onopen = () => {
+      //   socket.send(
+      //     `${JSON.stringify([
+      //       { ticket: uuidv4() }, //for websocket handshaking , uuid4 library 랜덤키 생성
+      //       { type: 'ticker', codes: webSocketParam }, // codes 요청 코인 문자열  (ex. KRW-BTC, BTC-ETH)
+      //     ])}`
+      //   );
+      // };
 
-      socket.onmessage = async (blob) => {
-        const websocketData = await new Response(blob.data).json();
+      // socket.onmessage = async (blob) => {
+      //   const websocketData = await new Response(blob.data).json();
 
-        // 변경 시킬 키값 업데이트
-        if (forUpdateCryptosObj[`${websocketData.code}`]) {
-          forUpdateCryptosObj[`${websocketData.code}`] = {
-            ...forUpdateCryptosObj[`${websocketData.code}`],
-            trade_price: websocketData['trade_price'],
-            change_rate: websocketData['change_rate'],
-            change_price: websocketData['change_price'],
-            acc_trade_price_24h: websocketData['acc_trade_price_24h'],
-          };
+      //   // 변경 시킬 키값 업데이트
+      //   if (forUpdateCryptosObj[`${websocketData.code}`]) {
+      //     forUpdateCryptosObj[`${websocketData.code}`] = {
+      //       ...forUpdateCryptosObj[`${websocketData.code}`],
+      //       trade_price: websocketData['trade_price'],
+      //       change_rate: websocketData['change_rate'],
+      //       change_price: websocketData['change_price'],
+      //       acc_trade_price_24h: websocketData['acc_trade_price_24h'],
+      //     };
 
-          let liveRenderCryptosArr = []; //웹소켓 변경 데이터 반영 배열, 렌더링 사용 위해
-          for (let coin in forUpdateCryptosObj) {
-            liveRenderCryptosArr.push(forUpdateCryptosObj[coin]);
-          }
+      //     let liveRenderCryptosArr = []; //웹소켓 변경 데이터 반영 배열, 렌더링 사용 위해
+      //     for (let coin in forUpdateCryptosObj) {
+      //       liveRenderCryptosArr.push(forUpdateCryptosObj[coin]);
+      //     }
 
-          setUpbitCryptos(liveRenderCryptosArr);
-        }
-      };
+      //     setUpbitCryptos(liveRenderCryptosArr);
+      //   }
+      // };
     } catch (err) {
       throw err;
     }
-  }, []);
+  }, [renderKRW]);
+
+  useEffect(async () => {
+    const socket = new WebSocket('wss://api.upbit.com/websocket/v1');
+
+    socket.onopen = () => {
+      socket.send(
+        `${JSON.stringify([
+          { ticket: uuidv4() }, //for websocket handshaking , uuid4 library 랜덤키 생성
+          { type: 'ticker', codes: websocketParams }, // codes 요청 코인 문자열  (ex. KRW-BTC, BTC-ETH)
+        ])}`
+      );
+    };
+
+    socket.onmessage = async (blob) => {
+      const websocketData = await new Response(blob.data).json();
+
+      const updatingUpbitCryptos = { ...initUpbitCryptos };
+
+      // 변경 시킬 키값 업데이트
+
+      if (updatingUpbitCryptos[`${websocketData.code}`]) {
+        updatingUpbitCryptos[`${websocketData.code}`] = {
+          ...updatingUpbitCryptos[`${websocketData.code}`],
+          trade_price: websocketData['trade_price'],
+          change_rate: websocketData['change_rate'],
+          change_price: websocketData['change_price'],
+          acc_trade_price_24h: websocketData['acc_trade_price_24h'],
+        };
+
+        let liveRenderCryptosArr = []; //웹소켓 변경 데이터 반영 배열, 렌더링 사용 위해
+        for (let coin in updatingUpbitCryptos) {
+          liveRenderCryptosArr.push(updatingUpbitCryptos[coin]);
+        }
+
+        setUpbitCryptos(liveRenderCryptosArr);
+      }
+    };
+  }, [websocketParams, initUpbitCryptos]);
 
   const switchColorHandler = (current) => {
     switch (current) {
@@ -164,8 +211,24 @@ const Popup = () => {
         <section>
           <img className="comoLogo" src={comoLogo}></img>
         </section>
-
-        <div></div>
+        <ul>
+          <li
+            className={renderKRW === 'KRW' ? 'currencyActive' : ''}
+            onClick={() => {
+              setRenderKRW('KRW');
+            }}
+          >
+            KRW
+          </li>
+          <li
+            className={renderKRW === 'BTC' ? 'currencyActive' : ''}
+            onClick={() => {
+              setRenderKRW('BTC');
+            }}
+          >
+            BTC
+          </li>
+        </ul>
       </nav>
       <main>
         <table>
@@ -177,7 +240,7 @@ const Popup = () => {
               <th>거래대금</th>
             </tr>
           </thead>
-          <tbody>{renderCryptosHandler()}</tbody>
+          <tbody>{renderKRW === 'KRW' ? renderCryptosHandler() : ''}</tbody>
         </table>
       </main>
       <footer>footer</footer>{' '}
