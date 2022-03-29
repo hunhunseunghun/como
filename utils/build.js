7a97a098ee97755a0d636085af1b21672bd7aa6e
hunhunseunghun <hunhunseunghun@gmail.com>
2022-03-29 16:07:44 +0900

    eslint

--- utils/build.js
+++ utils/build.js
@@ -2,6 +2,7 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
+process.env.COMO_API = '';
 
 var webpack = require('webpack'),
   config = require('../webpack.config');
