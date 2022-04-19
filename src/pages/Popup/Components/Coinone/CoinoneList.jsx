77528afcda41904daee863af1b1a2c193128216d
hunhunseunghun <hunhunseunghun@gmail.com>
2022-04-19 17:50:03 +0900

    coinlist

--- src/pages/Popup/Components/Coinone/CoinoneList.jsx
+++ src/pages/Popup/Components/Coinone/CoinoneList.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const CoinoneList = () => {
+  useEffect(() => {
+    console.log('coinone coninlist excuted');
+  }, []);
   return <div>CoinoneList</div>;
 };
 
